61811d1e1dbf10cbea8edf2ff2d67d08d66af52b
HARSH RAJ <harshraj250106@gmail.com>
2026-01-17 15:45:52 +0530

    push data

--- push_data.py
+++ push_data.py
@@ -49,8 +49,8 @@ class NetworkDataExtract():
             raise NetworkSecurityException(e,sys)
         
 if __name__=='__main__':
-    FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="KRISHAI"
+    FILE_PATH=os.path.join("Network_Data","phisingData.csv")
+    DATABASE="HARSHAI"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
