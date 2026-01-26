481f0669f430929d4ff24eeebde62d87e107ec8c
HARSH RAJ <harshraj250106@gmail.com>
2026-01-26 12:07:22 +0530

    network security

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,9 +1,4 @@
 import sys
-import os
-
-# Add parent directory to path to allow imports from the package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
